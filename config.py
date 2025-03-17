689742a6257ed8ede4aa39e18c9503082c2f7dbf
Mohammed3927 <gkhal839@gmail.com>
2025-03-18 02:40:18 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 import os
 from dotenv import load_dotenv
 
+# تحميل المتغيرات من .env
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 PREFIX = os.getenv("PREFIX", "!")
 GAME_CHANNELS = list(map(int, os.getenv("GAME_CHANNELS", "").split(",")))
 
+# الصلاحيات المطلوبة لتشغيل الألعاب
 REQUIRED_PERMISSIONS = ["administrator", "manage_events"]
