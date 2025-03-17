1710612fb23fcb94640b65fa1712fa3e7a6a4c04
Mohammed3927 <gkhal839@gmail.com>
2025-03-18 02:25:08 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,24 @@
+{
+    "token": "YOUR_BOT_TOKEN_HERE",
+    "prefix": "!",
+    "game_channels": ["game-channel-1", "game-channel-2"],
+    "game_permissions": {
+        "roulette": "manage_events",
+        "tic-tac-toe": "manage_events",
+        "mafia": "manage_events",
+        "musical_chairs": "manage_events",
+        "rock-paper-scissors": "manage_events",
+        "hide_and_seek": "manage_events",
+        "replica": "manage_events",
+        "button_game": "manage_events",
+        "fastest_typing": "manage_events",
+        "unscramble": "manage_events",
+        "merge_letters": "manage_events",
+        "flags": "manage_events",
+        "reverse_word": "manage_events",
+        "missing_letter": "manage_events",
+        "fix_the_word": "manage_events",
+        "number_sorting": "manage_events",
+        "emoji_match": "manage_events"
+    }
+}
